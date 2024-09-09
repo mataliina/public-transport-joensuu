340dc3722149d8d0021c7c696bdf9bd05043ef9c
Kaisa Heinonen <kaisa.hannamaarit@gmail.com>
2024-09-09 13:06:55 +0300

    edited dataUrls

--- src/utils/dataUrls.js
+++ src/utils/dataUrls.js
@@ -1,6 +1,3 @@
 export const TRIPUPDATE_DATA_URL = '/joensuu/api/gtfsrealtime/v1.0/feed/tripupdate';
-<<<<<<< HEAD
-=======
 export const STATIC_DATA_API_URL = 'api/gtfs/';
 export const GTFS_STATIC_PACKAGES_URL = 'https://tvv.fra1.digitaloceanspaces.com/207.zip'; // 207 = Joensuu
->>>>>>> b459e95 (add static packages url)
