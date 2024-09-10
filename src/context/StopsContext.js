e8cae970968dec9af6ded2a98060255c5c7edf0e
Kaisa Heinonen <kaisa.heinonen@siili.com>
2024-09-10 16:16:55 +0300

    fix useEffect missing dependencies in StopsContext

--- src/context/StopsContext.js
+++ src/context/StopsContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { useCallback, createContext, useEffect, useState } from 'react';
 import Papa from 'papaparse';
 
 export const StopsContext = createContext();
@@ -29,9 +29,70 @@ export const StopsProvider = ({ children }) => {
 			});
 	}, []);
 
+	const getTodaysStopTimesForStop = useCallback(
+		(stopId) => {
+			// get todays services
+			const today = new Date();
+			const dayOfWeek = today.getDay();
+			const formattedDate = today.toISOString().slice(0, 10);
+
+			let servicesToday = [];
+
+			const dayOfWeekToString = (dayOfWeek) => {
+				const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+				return days[dayOfWeek];
+			};
+
+			calendar.forEach((service) => {
+				if (new Date(getIsoDateString(service.start_date)) <= today && new Date(getIsoDateString(service.end_date)) >= today) {
+					if (service[dayOfWeekToString(dayOfWeek)] === '1') {
+						servicesToday.push(service.service_id);
+					}
+				}
+			});
+
+			calendarDates.forEach((service) => {
+				if (service.date === formattedDate.replace(/-/g, '')) {
+					if (service.exception_type === '1') {
+						servicesToday.push(service.service_id);
+					} else if (service.exception_type === '2') {
+						const index = servicesToday.indexOf(service.service_id);
+						if (index > -1) {
+							servicesToday.splice(index, 1);
+						}
+					}
+				}
+			});
+
+			//get todays stoptimes
+			const todaysTrips = trips.filter((trip) => servicesToday.includes(trip.service_id));
+
+			const todaysStopTimes = stopTimes.filter((stopTime) => todaysTrips.some((trip) => trip.trip_id === stopTime.trip_id));
+
+			//get todays stop times for this stop
+			const stopTimesForStop = todaysStopTimes
+				.filter((row) => row.stop_id === stopId)
+				.map((stopTime) => {
+					const trip = trips.find((trip) => trip.trip_id === stopTime.trip_id);
+					return {
+						...stopTime,
+						routeId: trip ? trip.route_id : null,
+						directionId: trip ? trip.direction_id : null,
+					};
+				});
+
+			const sortedStopTimes = stopTimesForStop.sort((a, b) => {
+				return new Date(`1970-01-01T${a.departure_time}Z`).getTime() - new Date(`1970-01-01T${b.departure_time}Z`).getTime();
+			});
+			setStopTimesData(sortedStopTimes);
+			return sortedStopTimes;
+		},
+		[trips, calendar, calendarDates, stopTimes]
+	);
+
 	useEffect(() => {
 		getTodaysStopTimesForStop(selectedStop);
-	}, [selectedStop]);
+	}, [selectedStop, getTodaysStopTimesForStop]);
 
 	const getIsoDateString = (dateString) => {
 		return dateString.slice(0, 4) + '-' + dateString.slice(4, 6) + '-' + dateString.slice(6, 8);
@@ -137,79 +198,13 @@ export const StopsProvider = ({ children }) => {
 		}
 	};
 
-	const getTodaysServices = () => {
-		const today = new Date();
-		const dayOfWeek = today.getDay();
-		const formattedDate = today.toISOString().slice(0, 10);
-
-		let servicesToday = [];
-
-		const dayOfWeekToString = (dayOfWeek) => {
-			const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-			return days[dayOfWeek];
-		};
-
-		calendar.forEach((service) => {
-			if (new Date(getIsoDateString(service.start_date)) <= today && new Date(getIsoDateString(service.end_date)) >= today) {
-				if (service[dayOfWeekToString(dayOfWeek)] === '1') {
-					servicesToday.push(service.service_id);
-				}
-			}
-		});
-
-		calendarDates.forEach((service) => {
-			if (service.date === formattedDate.replace(/-/g, '')) {
-				if (service.exception_type === '1') {
-					servicesToday.push(service.service_id);
-				} else if (service.exception_type === '2') {
-					const index = servicesToday.indexOf(service.service_id);
-					if (index > -1) {
-						servicesToday.splice(index, 1);
-					}
-				}
-			}
-		});
-
-		return servicesToday;
-	};
-
-	const getTodaysStopTimes = (servicesToday) => {
-		const todaysTrips = trips.filter((trip) => servicesToday.includes(trip.service_id));
-
-		return stopTimes.filter((stopTime) => todaysTrips.some((trip) => trip.trip_id === stopTime.trip_id));
-	};
-
-	const getTodaysStopTimesForStop = (stopId) => {
-		const servicesToday = getTodaysServices();
-		const todaysStopTimes = getTodaysStopTimes(servicesToday);
-
-		const stopTimesForStop = todaysStopTimes
-			.filter((row) => row.stop_id === stopId)
-			.map((stopTime) => {
-				const trip = trips.find((trip) => trip.trip_id === stopTime.trip_id);
-				return {
-					...stopTime,
-					routeId: trip ? trip.route_id : null,
-					directionId: trip ? trip.direction_id : null,
-				};
-			});
-
-		const sortedStopTimes = stopTimesForStop.sort((a, b) => {
-			return new Date(`1970-01-01T${a.departure_time}Z`).getTime() - new Date(`1970-01-01T${b.departure_time}Z`).getTime();
-		});
-		setStopTimesData(sortedStopTimes);
-		return sortedStopTimes;
-	};
-
 	const getStopName = (stopId) => {
 		const currentStop = stops.find((stop) => stop.stop_id === stopId);
 		return currentStop ? currentStop.stop_name : 'Stop name not found';
 	};
 
 	return (
-		<StopsContext.Provider
-			value={{ stops, getStopName, getTodaysServices, fetchStops, selectedStop, setSelectedStop, stopTimesData, loading }}
-		>
+		<StopsContext.Provider value={{ stops, getStopName, fetchStops, selectedStop, setSelectedStop, stopTimesData, loading }}>
 			{children}
 		</StopsContext.Provider>
 	);
